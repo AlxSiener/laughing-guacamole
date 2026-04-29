359af1f02433f10c5c905b7d3449211e9e04c406
asiener <siener.alex@icloud.com>
2026-04-29 19:56:06 -0400

    Initial Commit

---
+++ Task 31/Task_31App.swift
@@ -0,0 +1,17 @@
+//
+//  Task_31App.swift
+//  Task 31
+//
+//  Created by Alexander Siener on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Task_31App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
