5bf355830d0af08e2a0a7725bd630e36e0e7fa4b
asiener <siener.alex@icloud.com>
2026-04-29 20:20:58 -0400

    Added station view

---
+++ Task 31/StationView.swift
@@ -0,0 +1,18 @@
+//
+//  StationView.swift
+//  Task 31
+//
+//  Created by Alexander Siener on 4/29/26.
+//
+
+import Foundation
+import SwiftUI
+
+struct StationView: View {
+    var body: some View {
+        Image(systemName: "house.and.flag")
+            .foregroundStyle(Color.brown)
+            .font(.system(size: 100))
+    }
+}
+
