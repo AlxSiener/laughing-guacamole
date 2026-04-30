e3734aabdf80142a040a368287b99d34498a56c5
asiener <siener.alex@icloud.com>
2026-04-29 20:05:06 -0400

    Added train view

---
+++ Task 31/Train View.swift
@@ -0,0 +1,26 @@
+//
+//  Train View.swift
+//  Task 31
+//
+//  Created by Alexander Siener on 4/29/26.
+//
+
+import Foundation
+import SwiftUI
+struct TrainView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "train.side.rear.car")
+            Image(systemName: "train.side.middle.car")
+            Image(systemName: "train.side.front.car")
+               
+        }
+        .font(.system(size: 80))
+        .foregroundStyle(Color.blue)
+    
+    }
+}
+
+#Preview {
+    TrainView()
+}
